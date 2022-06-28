9da2f56e75096f6515d510047be2955957782bea
yuxiang-13 <1270991359@qq.com>
2022-06-28 15:03:36 +0800

    111

--- Source/ThirdProject/ThirdProject.Build.cs
+++ Source/ThirdProject/ThirdProject.Build.cs
@@ -8,6 +8,12 @@ public class ThirdProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay",
+			"GameplayAbilities",
+			"GameplayTags",
+			"GameplayTasks"
+		});
 	}
 }
