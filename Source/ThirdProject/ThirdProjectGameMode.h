6fec6e8f80b5eeb167ce2ed34dceea7308434f49
yuxiang-13 <1270991359@qq.com>
2022-06-28 14:16:14 +0800

    111

---
+++ Source/ThirdProject/ThirdProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThirdProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AThirdProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AThirdProjectGameMode();
+};
+
+
+
