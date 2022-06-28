e7e78db600e0daab5386d869e930555dcc0d9aa4
yuxiang-13 <1270991359@qq.com>
2022-06-28 19:10:38 +0800

    ag调用task

--- Source/ThirdProject/Private/Ability/RPGGameplayAbility.cpp
+++ Source/ThirdProject/Private/Ability/RPGGameplayAbility.cpp
@@ -3,6 +3,9 @@
 
 #include "Ability/RPGGameplayAbility.h"
 
+#include "Ability/Task/RPGAbilityTask_PMAW.h"
+#include "ThirdProject/ThirdProjectCharacter.h"
+
 URPGGameplayAbility::URPGGameplayAbility()
 {
 	
@@ -33,13 +36,29 @@ void URPGGameplayAbility::OnCancelled()
 void URPGGameplayAbility::OnDamageGameplayEvent(FGameplayTag InGameplayTag, FGameplayEventData Payload)
 {
 	// 最后 GA 调用 Attribute
+	
 }
 
+// 激活某个能力
 void URPGGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
 	const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	// 开始执行 能力，失败 直接返回
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		return;
+	}
+	AThirdProjectCharacter * Character = Cast<AThirdProjectCharacter>(ActorInfo->OwnerActor)
+	if (Character)
+	{
+		// 成功,播放 蒙太奇
+		if (PlayMontage(*FString::FromInt(0)))
+		{
+			
+		}
+	}
 	
 }
 
@@ -51,13 +70,41 @@ void URPGGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle,
 	
 }
 
+// 创建 UAbilityTask
 UAbilityTask_PlayMontageAndWait* URPGGameplayAbility::CreatePlayMontageAndWaitProxy(FName TaskInstanceName,
 	UAnimMontage* InMontageToPlay, float Rate, FName StartSection, bool bStopWhenAbilityEnds,
 	float AnimRootMotionTranslationScale, float StartTimeSeconds)
 {
-	
+	URPGAbilityTask_PMAW* InWait = URPGAbilityTask_PMAW::CreatePMAWDamageEventProxy(
+		this,
+		TaskInstanceName,
+		InMontageToPlay,
+		AbilityTags,
+		Rate,
+		StartSection,
+		bStopWhenAbilityEnds,
+		AnimRootMotionTranslationScale,
+		StartTimeSeconds
+	);
+
+	// 任务成功, 把GA函数绑定到task
+	if (InWait)
+	{
+		InWait->OnCompleted.AddDynamic(this, &URPGGameplayAbility::OnCompleted);
+		InWait->OnCompleted.AddDynamic(this, &URPGGameplayAbility::OnBlendOut);
+		InWait->OnCompleted.AddDynamic(this, &URPGGameplayAbility::OnInterrupted);
+		InWait->OnCompleted.AddDynamic(this, &URPGGameplayAbility::OnCancelled);
+		// 西药知道 task 执行完毕
+		InWait->DamageEventReceived.AddDynamic(this, &URPGGameplayAbility::OnDamageGameplayEvent);
+
+		// task创建后了 参数也好了
+		InWait->Activate();
+		return  InWait;
+	}
+	return  NULL;
 }
 
+// task告诉执行蒙太奇
 UAbilityTask_PlayMontageAndWait* URPGGameplayAbility::PlayMontage(FName StartSection)
 {
 	return CreatePlayMontageAndWaitProxy(NAME_None, MontageToPlay, 1.0f, StartSection);
--- Source/ThirdProject/Private/Ability/Task/RPGAbilityTask_PMAW.cpp
+++ Source/ThirdProject/Private/Ability/Task/RPGAbilityTask_PMAW.cpp
@@ -3,3 +3,34 @@
 
 #include "Ability/Task/RPGAbilityTask_PMAW.h"
 
+#include "AbilitySystemGlobals.h"
+
+URPGAbilityTask_PMAW::URPGAbilityTask_PMAW()
+{
+}
+
+void URPGAbilityTask_PMAW::Activate()
+{
+	Super::Activate();
+}
+
+void URPGAbilityTask_PMAW::OnDestroy(bool bInOwnerFinished)
+{
+	Super::OnDestroy(bInOwnerFinished);
+}
+
+URPGAbilityTask_PMAW* URPGAbilityTask_PMAW::CreatePMAWDamageEventProxy(UGameplayAbility* OwningAbility,
+	FName TaskInstanceName, UAnimMontage* MontageToPlay, FGameplayTagContainer InEventTags, float Rate,
+	FName StartSection, bool bStopWhenAbilityEnds, float AnimRootMotionTranslationScale, float StartTimeSeconnds)
+{
+	// 应用能力
+	UAbilitySystemGlobals::NonShipping_ApplyGlobalAbilityScaler_Rate(Rate);
+	// 创建 能力
+	URPGAbilityTask_PMAWDamageEvent* MyObj = NewAbilityTask<URPGAbilityTask_PMAWDamageEvent>(OwningAbility, TaskInstanceName);
+	
+	return NULL;
+}
+
+void URPGAbilityTask_PMAW::OnDamageGameplayEvent(FGameplayTag InGameplayTag, const FGameplayEventData* Payload)
+{
+}
